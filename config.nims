769071b31cafdb32d60cc9bb1e24daed41d739a9
Jake Leahy <jakeleahy@gmail.com>
2020-08-26 19:45:11 +1000

    Added reaction based pageination

--- config.nims
+++ config.nims
@@ -1,3 +1,4 @@
 switch("d", "ssl")
-switch("forceBuild") # So it doesn't cache git version
+# switch("forceBuild") # So it doesn't cache git version
+switch("d", "nimWorkaround14447")
 # switch("d", "dimscordDebug")
--- src/doccat.nim
+++ src/doccat.nim
@@ -10,6 +10,7 @@ import regex
 import tables
 import options
 import strformat
+import wait
 
 proc buildDocTable(): Table[string, Entry] =
     # HTML to markdown regex
@@ -50,19 +51,30 @@ else:
 const 
     docTable = buildDocTable()
     dimscordVersion = readFile("dimscord/version")
-    
+    forwardEmoji = "➡️"
+    backEmoji = "⬅️"
 let discord = newDiscordClient(token)
 
-template reply(m: Message, content: string, messageEmbed: Option[Embed] = none(Embed)): untyped =
-    discard await discord.api.sendMessage(m.channelId, content, embed = messageEmbed)
+proc reply(m: Message, content: string, messageEmbed: Option[Embed] = none(Embed)): Future[Message] {.async.}=
+    return await discord.api.sendMessage(m.channelId, content, embed = messageEmbed)
 
 proc trunc(s: string, length: int, page: int = 0): string =
     # TODO paginiation
     if s.len() > length:
         var wordEnd = length * (page + 1)
         if wordEnd > s.len(): wordEnd = s.len()
-        return s[(page * length) + (if page > 0: -5 else: 0)..<wordEnd] & (if wordEnd < s.len(): fmt" ( run command again with `{page + 1}` at the end to get more)" else: "")
+        return s[(page * length) + (if page > 0: -5 else: 0)..<wordEnd]
     return s    
+
+
+
+discord.events.onDispatch = proc (s: Shard, evt: string, data: JsonNode) {.async.} =
+    if evt == "MESSAGE_REACTION_ADD":
+        # If it is an emoji then check if a message is waiting on a reaction
+        if waits.hasKey(data["message_id"].str):
+            let wait = waits[data["message_id"].str]
+            wait.complete(data["emoji"]["name"].str)
+            waits.del(data["message_id"].str)
 
 discord.events.message_create = proc (s: Shard, m: Message) {.async.} =
     if m.author.bot and not m.webhookId.isSome(): return
@@ -71,18 +83,22 @@ discord.events.message_create = proc (s: Shard, m: Message) {.async.} =
     # TODO search
     if unlikely(args[0] == "doc"):
         if args.len() == 1:
-            m.reply("You have not specified a name")
+            discard m.reply("You have not specified a name")
         else:
             let name = args[1].toLower()
             if name == "help":
-                m.reply("to use, just send `doc` followed by something in the library e.g. `doc sendMessage`\nFor big things like `doc Events` you can tack a number onto the end to get more `doc Events 2`")
+                discard m.reply("to use, just send `doc` followed by something in the library e.g. `doc sendMessage`\nFor big things like `doc Events` you can tack a number onto the end to get more `doc Events 2`")
                 return
             var page = 0
+            
             if args.len() >= 3:
                 page = abs(parseInt(args[2]) - 1)
             if docTable.hasKey(name):
                 let entry = docTable[name]
-                    
+                var maxPage = int ceil(len(entry.code)/1500)
+                var 
+                    responseMessage: Message
+                echo(maxPage, page)
                 if entry.description.isSome:
                     let description = entry.description.get()
                     let embed = some Embed(
@@ -90,12 +106,42 @@ discord.events.message_create = proc (s: Shard, m: Message) {.async.} =
                         description: entry.description,
                         url: some fmt"https://github.com/krisppurg/dimscord/blob/{dimscordVersion}/{entry.file.get()}#L{entry.line}"
                     )
-                    m.reply(&"```nim\n{entry.code.trunc(1500, page)}```", embed)
-    
+                    responseMessage = await m.reply(&"```nim\n{entry.code.trunc(1500, page)}```", embed)
                 else:
-                    m.reply(&"```nim\n{entry.code.trunc(1500, page)}```")
+                    responseMessage = await m.reply(&"```nim\n{entry.code.trunc(1500, page)}```")
+                if entry.code.len > 1500:
+                    while true:
+                        if page > 0:
+                            await discord.api.addMessageReaction(m.channelId, responseMessage.id, backEmoji)
+                        if page + 1 < maxPage:
+                            await discord.api.addMessageReaction(m.channelId, responseMessage.id, forwardEmoji)
+                        let reaction = await responseMessage.waitForReaction()
+                        if reaction == backEmoji:
+                            if page != 0:
+                                page -= 1
+                        elif reaction == forwardEmoji:
+                            if page + 1 < maxPage:
+                                page += 1
+                        elif reaction.isEmptyOrWhiteSpace: # Emojis make it blank
+                            echo("timed out")
+                            return
+                        if entry.description.isSome:
+                            let description = entry.description.get()
+                            let embed = some Embed(
+                            title: some entry.name,
+                                description: entry.description,
+                                url: some fmt"https://github.com/krisppurg/dimscord/blob/{dimscordVersion}/{entry.file.get()}#L{entry.line}"
+                            )
+                            discard await discord.api.editMessage(responseMessage.channelId, responseMessage.id, &"```nim\n{entry.code.trunc(1500, page)}```", embed = embed)                                
+                        else:
+                            discard await discord.api.editMessage(responseMessage.channelId, responseMessage.id, &"```nim\n{entry.code.trunc(1500, page)}```")
+                        try:
+                            await discord.api.deleteAllMessageReactions(m.channelId, responseMessage.id)
+                        except:
+                            echo("no permission")
+                            # discard await m.reply("I don't have permission to manage messages")
             else:
-                m.reply("I'm sorry, but there is nothing with this name")
+                discard m.reply("I'm sorry, but there is nothing with this name")
 discord.events.on_ready = proc (s: Shard, r: Ready) {.async.} =
     echo "Ready as " & $r.user
     
